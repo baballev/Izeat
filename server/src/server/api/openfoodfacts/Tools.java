bbf1d9b9f5adf6bbbd0bd4ece5e95ae3463e5dee
Vincent Josse <vincent.josse@telecom-paris.fr>
2019-11-09 19:29:18 +0100

    Server package structure + Tools interface for OpenFoodFacts

---
+++ server/src/server/api/openfoodfacts/Tools.java
@@ -0,0 +1,13 @@
+package server.api.openfoodfacts;
+
+import java.net.HttpURLConnection;
+import java.lang.StringBuilder;
+
+public class Tools implements ToolsInterface{
+
+	public static StringBuilder getProductQuery(int code) {
+		
+		return null;
+	}
+	
+}
