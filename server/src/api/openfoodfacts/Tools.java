1fde5ae66805e9eab5f95d49edfe1b88cd272af9
David Serge Chistian <davidgerard@MacBook-Pro-de-David.local>
2019-11-18 16:24:54 +0100

    Committed from david's computer, need to check with doc at home

--- server/src/api/openfoodfacts/Tools.java
+++ server/src/api/openfoodfacts/Tools.java
@@ -1,11 +1,37 @@
 package api.openfoodfacts;
 
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import java.io.IOException;
 import java.lang.StringBuilder;
 
 public class Tools implements ToolsInterface{
 
 	public static StringBuilder getProductQuery(int code) {
+		
+		String url = "https://world.openfoodfacts.org/api/";
+		
+		try {
+			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("User-Agent", "Java server IzEat"); // TODO: Check that
+			
+			StringBuilder content;
+			
+			try {	
+				content = new StringBuilder();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			connection.disconnect();
+		}
 		
 		return null;
 	}
