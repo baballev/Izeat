c972aebfa17105db8f60058067ff341e1f065d0b
Vincent Josse <vincent.josse@telecom-paris.fr>
2019-11-18 22:02:33 +0100

    Completing the structure of the Product class

--- server/src/Main.java
+++ server/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String code = "3017620402678";
+		String code = "3482970008397";
 		Product product = new Product(code);
 		System.out.println(product.getName());
 		
--- server/src/api/openfoodfacts/Product.java
+++ server/src/api/openfoodfacts/Product.java
@@ -5,23 +5,53 @@ import java.util.HashMap;
 import org.json.*;
 
 public class Product {
-				  
-	private final JSONObject jsonInfo;
+	
+	/* General notes:
+	 * The class was built to be easily convertible to a .JSON file:
+	 * if product is instanced by this class,
+	 * JSONObject obj = new JSONObject(product);
+	 * String json = obj.toString();
+	 * will give json the structure of a .JSON file, which means it is ready to be transferred.
+	 * For every getter getAttribute() in the class, there will be 
+	 * "attribute": getAttribute() 
+	 * in json.
+	 * For example: "name": "Nutella"
+	 */
+	
+	
+	private final String name;		 // Example: "Nutella"	
+	private final String quantity;	 // Example: "100 g"
+	private final String imageUrl;	 // Example: "https://static.openfoodfacts.org/images/products/301/762/040/2678/front_fr.77.400.jpg"
+	private int novaScore;			 // Example: 1
+	private final char nutriScore;	 // Example: 'c'
 	
 	public Product(StringBuilder sb) {  // sb corresponds to the json file.
 		String str = sb.toString();
-		this.jsonInfo = new JSONObject(str).getJSONObject("product");
-		}
-	
-	public Product(String code) { // code represents the 12 digits bar-code.
+		JSONObject jsonInfo = new JSONObject(str).getJSONObject("product");
+		
+		if (jsonInfo.getString("product_name_fr") != "") name = jsonInfo.getString("product_name_fr");
+		else name = jsonInfo.getString("product_name");
+		quantity = jsonInfo.getString("quantity");
+		imageUrl = jsonInfo.getString("image_url");
+		try {novaScore = Integer.parseInt(jsonInfo.getString("nova_group"));}
+		catch(JSONException e) {novaScore = 0;}
+		nutriScore = jsonInfo.getString("nutrition_grades").charAt(0);
+	}
+	public Product(String code) {  // code represents the 12 digits bar-code.
 		StringBuilder productInfo = Tools.getProductQuery(code);
 		String str = productInfo.toString();
-		this.jsonInfo = new JSONObject(str).getJSONObject("product");
-				
+		JSONObject jsonInfo = new JSONObject(str).getJSONObject("product");
+		if (jsonInfo.getString("product_name_fr") != "") name = jsonInfo.getString("product_name_fr");
+		else name = jsonInfo.getString("product_name");
+		quantity = jsonInfo.getString("quantity");
+		imageUrl = jsonInfo.getString("image_url");
+		try {novaScore = Integer.parseInt(jsonInfo.getString("nova_group"));}
+		catch(JSONException e) {novaScore = 0;}
+		nutriScore = jsonInfo.getString("nutrition_grades").charAt(0);
 	}
 	
-	public String getName() { // Returns the french name of the product
-		return this.jsonInfo.getString("product_name_fr").toString();
+	public String getName() { // Returns the french name of the product if available, and the english one otherwise.
+		return name;
 	}
 	
 	public HashMap<String, Float> getNutriment() {
