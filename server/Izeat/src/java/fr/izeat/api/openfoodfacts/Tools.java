8cb84eebde1c40fc8aa1329a373373772d15755f
Vincent Josse <vincent.josse@telecom-paris.fr>
2019-11-27 20:45:47 +0100

    Test getSearchQuery

--- server/Izeat/src/java/fr/izeat/api/openfoodfacts/Tools.java
+++ server/Izeat/src/java/fr/izeat/api/openfoodfacts/Tools.java
@@ -13,7 +13,6 @@ import java.lang.StringBuilder;
 public class Tools implements ToolsInterface {
 
 	public static StringBuilder getProductQuery(String code) {
-
 		String url = "http://fr.openfoodfacts.org/api/v0/product/" + code + ".json"; // Problèmes si on met en https car les certificats ne sont pas valides, TODO: voir comment configurer les certifs ssl
 		StringBuilder content = null;
 		try {
@@ -35,19 +34,33 @@ public class Tools implements ToolsInterface {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-
+                }
 		return content;  // Returns a StringBuilder Object which can be interpreted using .toString() method which corresponds to the .JSON file content.
 	}
 
-	
-	public static String[] getIngredients(String code) {
-		
-		
-		
-		return null;
-	}
-	
-	
-	
+    public static StringBuilder getSearchQuery(String query, int pageNumber, int pageSize) {
+        //TODO: implement pageNumber and pageSize, tags, ...
+        String url = "https://world.openfoodfacts.org/cgi/search.pl?search_terms=" + query + "&search_simple=1&action=process&json=1";
+        StringBuilder content = null;
+        try{
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+            try{
+                BufferedReader input = new BufferedReader(new InputStreamreader(connection.getInputStream(), "UTF-8"));
+                content = new StringBuilder();
+                String line;
+                while((line = input.readLine()) != null){
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return content;
+    }
 }
+
+
