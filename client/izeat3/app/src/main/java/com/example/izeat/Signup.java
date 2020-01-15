7106ee004bb0f01d69c8a2e12d8b2b0273021d1b
Lea Zerah <leazerah@MacBook-Air-de-Lea-2.local>
2020-01-15 16:07:56 +0100

    changements de noms

---
+++ client/izeat3/app/src/main/java/com/example/izeat/Signup.java
@@ -0,0 +1,36 @@
+package com.example.izeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class Signup extends AppCompatActivity {
+
+    private TextView welcome;
+    private TextView conection;
+    private EditText mail;
+    private EditText password;
+    private Button signup;
+    private Button passwordforgotten;
+    private Button next;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        welcome=(TextView) findViewById(R.id.activity_main_welcome_txt);
+        conection=(TextView) findViewById(R.id.activity_main_conection_txt);
+        mail=(EditText) findViewById(R.id.activity_main_mail_input);
+        password=(EditText) findViewById(R.id.activity_main_password_input);
+        signup=(Button) findViewById(R.id.activity_main_signup_btn);
+        passwordforgotten=(Button) findViewById(R.id.activity_passwordforgotten_btn);
+        next=(Button) findViewById(R.id.activity_main_next_btn);
+    }
+
+}
