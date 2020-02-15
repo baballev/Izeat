473f47846571ae5510529f9b369e8a60a1db1d92
Vincent Josse <baballevincent@gmail.com>
2020-02-15 17:06:32 +0100

    Added instruction in comments for CNN integration in android

--- server/engine/ResNet50 fine tuning dev.py
+++ server/engine/ResNet50 fine tuning dev.py
@@ -93,12 +93,42 @@ else:
     model = Sequential()
     model.add(restnet)
     model.add(Dense(numClasses, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.SGD(0.01), metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.SGD(0.01), metrics=['accuracy']) # Try Adadelta instead of SGD as it is an adaptative learning rate optimizer
     model.summary() # Display trainable layers
 
 if needTrain:
     history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=nb_of_epochs, validation_steps=50, verbose=1)
     model.save(weights_file)
+# TODO : Save for android:
+"""
+https://www.tensorflow.org/lite
+https://www.youtube.com/watch?v=kFWKdLOxykE&feature=youtu.be&t=1019&fbclid=IwAR0XrStORn0UXwVy7D2Ea-nQtSY9K0YiWplV2r7eYBronkm3fb1MGEPJG9E
+    https://github.com/tensorflow/tensorflow/tree/master/tensorflow/examples/android
+def export_model(saver, model, input_node_names, output_node_name):
+    tf.train.write_graph(K.get_session().graph_def, 'out', \
+        MODEL_NAME + '_graph.pbtxt')
+
+    saver.save(K.get_session(), 'out/' + MODEL_NAME + '.chkp')
+
+    freeze_graph.freeze_graph('out/' + MODEL_NAME + '_graph.pbtxt', None, \
+        False, 'out/' + MODEL_NAME + '.chkp', output_node_name, \
+        "save/restore_all", "save/Const:0", \
+        'out/frozen_' + MODEL_NAME + '.pb', True, "")
+
+    input_graph_def = tf.GraphDef()
+    with tf.gfile.Open('out/frozen_' + MODEL_NAME + '.pb', "rb") as f:
+        input_graph_def.ParseFromString(f.read())
+
+    output_graph_def = optimize_for_inference_lib.optimize_for_inference(
+            input_graph_def, input_node_names, [output_node_name],
+            tf.float32.as_datatype_enum)
+
+    with tf.gfile.FastGFile('out/opt_' + MODEL_NAME + '.pb', "wb") as f:
+        f.write(output_graph_def.SerializeToString())
+
+    print("graph saved!")
+"""
+
 else: # predict tests
     x_test = np.zeros((25, img_size, img_size, depth), dtype=np.float32)
     for i in range(25):
