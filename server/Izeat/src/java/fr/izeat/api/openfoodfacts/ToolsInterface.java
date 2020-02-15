7a9c012ea1dad740d879039811fcb4b998b69310
Vincent Josse <vincent.josse@telecom-paris.fr>
2020-02-15 16:13:21 +0100

    Update server/Izeat/src/java/fr/izeat/api/openfoodfacts/ToolsInterface.java

--- server/Izeat/src/java/fr/izeat/api/openfoodfacts/ToolsInterface.java
+++ server/Izeat/src/java/fr/izeat/api/openfoodfacts/ToolsInterface.java
@@ -8,7 +8,7 @@ public interface ToolsInterface {
      * a  String. This String contains the JSON file's content.
      * What it does not: Format data so that it is ready to be used. See Product class for that purpose.
      * 
-     * code is a 12 digits integer corresponding to the barcode of the desired
+     * code is a 12-13 digits integer corresponding to the barcode of the desired
      * product. It is represented as a string as every 0 at the begining matters.
      * Returns a String containing the JSON file.
      * If the method fails to get the JSON file, it will return null.
@@ -20,8 +20,8 @@ public interface ToolsInterface {
     /* query corresponds to the search field.
      * pageNumber corresponds to the desired page number of the research result.
      * pageSize corresponds to the number of products by page.
-     * Returns a String containing the HTML doc.
-     * If the method fails to get the HTML doc, it will return null.
+     * Returns a String containing the JSON file with every products that matched the research terms.
+     * If the method fails to get the JSON doc, it will return null.
      */
     public static String getSearchQuery(String query, int pageNumber, int pageSize) {
         return null;
