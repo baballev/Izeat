cbdccd6386d9829369fe388401bb2959888d7cfc
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2020-02-20 16:14:46 +0100

    renamed class recipe + fix deletion of activity Login and Sign Up

---
+++ client/izeat3/app/src/main/java/com/example/izeat/Controller/SignUp.java
@@ -0,0 +1,24 @@
+package com.example.izeat.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.izeat.R;
+
+public class SignUp extends AppCompatActivity {
+
+
+    private TextView signup;
+    private TextView sentance;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+    }
+}
