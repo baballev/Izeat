54dfa41b97875ff3b5ac9fe806bd894df9eda16f
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2020-02-24 17:01:50 +0100

    minor change

--- client/izeat3/app/src/main/java/com/example/izeat/Controller/ProductsRecomendationActivity.java
+++ client/izeat3/app/src/main/java/com/example/izeat/Controller/ProductsRecomendationActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.izeat.R;
+
 public class ProductsRecomendationActivity extends AppCompatActivity {
 
     @Override
