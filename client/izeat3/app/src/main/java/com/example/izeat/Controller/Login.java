e2672c2629200176f5e0a89bbb180643e9c48774
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2020-03-09 21:00:42 +0100

    test implementation of JSON lib with products
    Former-commit-id: 9e51e05efdaf0acce4cde198f8c272b8a058ae05

--- client/izeat3/app/src/main/java/com/example/izeat/Controller/Login.java
+++ client/izeat3/app/src/main/java/com/example/izeat/Controller/Login.java
@@ -10,7 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.izeat.Model.Product;
 import com.example.izeat.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Login extends AppCompatActivity {
@@ -35,6 +39,20 @@ public class Login extends AppCompatActivity {
                 finish();
             }
         });
+
+        String s = Product.getProductQuery("3017760589895");
+        s.length();
+        JSONObject jsonProduct = null;
+        try {
+            jsonProduct = new JSONObject(s);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(jsonProduct.getString("quantity"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         inscription = findViewById(R.id.btninscription);
         inscription.setOnClickListener(new View.OnClickListener(){
--- client/izeat3/app/src/main/java/com/example/izeat/Model/Product.java
+++ client/izeat3/app/src/main/java/com/example/izeat/Model/Product.java
@@ -1,5 +1,12 @@
 package com.example.izeat.Model;
 
+import android.os.Build;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Product {
     private String productName;
     private String productUrl;
@@ -15,6 +22,48 @@ public class Product {
 
     public String getProductUrl() {
         return productUrl;
+    }
+
+    public static String getProductQuery(String code) {
+                /*try{
+                    InputStream content=new FileInputStream("off_product.txt");
+                    InputStreamReader lecture=new InputStreamReader(content);
+                    BufferedReader buff=new BufferedReader(lecture);
+                    String line;
+                    line=buff.readLine();
+                    buff.close();
+
+                }catch(Exception e){
+                    System.out.println(e.toString());
+                }*/
+
+        String url = "http://izeat.r2.enst.fr/ws/Izeat/webresources/product/" + code + ".json"; // Problèmes si on met en https car les certificats ne sont pas valides, TODO: voir comment configurer les certifs ssl
+        StringBuilder content = null;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET"); // Could throw ProtocolException and SecurityException.
+            // connection.setRequestProperty("User-Agent", "Java server IzEat");
+            try {
+                BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8")); // Could throw IOException
+                content = new StringBuilder();
+                String line;
+                while ((line = input.readLine()) != null) {  // In fact there should be only 1 line for a .JSON file.
+                    content.append(line);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        content.append(System.lineSeparator());
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                connection.disconnect();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (content !=null){
+            return content.toString(); }// Returns a StringBuilder Object which can be interpreted using .toString() method which corresponds to the .JSON file content.
+        else return null;
     }
 
 }
