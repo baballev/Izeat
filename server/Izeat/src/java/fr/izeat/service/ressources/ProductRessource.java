a881a80ccf13a51b9d2fc2d98c56be1084780ec1
Vincent Josse <vincent.josse@telecom-paris.fr>
2020-03-11 15:50:20 +0100

    Changing return methods to send real json files

--- server/Izeat/src/java/fr/izeat/service/ressources/ProductRessource.java
+++ server/Izeat/src/java/fr/izeat/service/ressources/ProductRessource.java
@@ -6,23 +6,24 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import org.json.JSONObject;
 
 @Path("/product/{code}") // Code is a 12-13 digits number representing the bar-code of the queried procuct
 public class ProductRessource { 
     
     @GET
-    @Produces("text/json")
-    public String getProduct(@PathParam("code") String code){
+    @Produces("application/json")
+    public Response getProduct(@PathParam("code") String code){
         try{
         String sb = Tools.getProductQuery(code); // Fetch data from OpenFoodFacts
         Product product = new Product(sb);          // Create a corresponding product object (Format data)0
         JSONObject jsonProduct = new JSONObject(product);
-        return jsonProduct.toString();// Transfer .JSON file
+        return Response.ok(jsonProduct.toString(), MediaType.APPLICATION_JSON).build();// Transfer .JSON file
         }catch(Exception e){
             e.printStackTrace();
-        }
-            
+        }     
         return null;
     }
 }    
