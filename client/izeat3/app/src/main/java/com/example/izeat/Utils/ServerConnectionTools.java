8af304d73466e0d14e68302b10f27c344085109d
Vincent Josse <vincent.josse@telecom-paris.fr>
2020-03-11 13:58:43 +0100

    Reworked server get tools + started doc + added search query
    Former-commit-id: fcc54dc156d1879e9e0bf540f53f70ec77fad47b

--- client/izeat3/app/src/main/java/com/example/izeat/Utils/ServerConnectionTools.java
+++ client/izeat3/app/src/main/java/com/example/izeat/Utils/ServerConnectionTools.java
@@ -11,28 +11,45 @@ import com.android.volley.toolbox.Volley;
 
 public final class ServerConnectionTools {
 
-    public static String getProduct(Context context, String barcode){
+    public static String getRequestServer(Context context, String url){
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(context);
-        String url ="http://izeat.r2.enst.fr/ws/Izeat/webresources/product/" + barcode;
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        System.out.println("Response recieved correctly: "+ response.toString());
+                        System.out.println("Response received correctly: "+ response.toString());
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                System.out.println(error.getMessage());
+                System.err.println(error.getMessage());
             }
         });
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
         return stringRequest.toString();
+    }
+    /*
+     * Method Usage:
+     *  Parameters:
+     *      context: Give the current context of the activity (use getApplicationContext())
+     *      barcode: /!\ String /!\ The barcode of the desired product information
+     *  Return value:
+     *      Returns a String containing all the json file related to the product infos
+     *  Example:
+     *      getProduct(getApplicationContext, "3017760589895");
+     *      -> Gives information about Pépito mini rollos.
+     */
+    public static String getProduct(Context context, String barcode){
+        String url = "http://izeat.r2.enst.fr/ws/Izeat/webresources/product/" + barcode;
+        return getRequestServer(context, url);
 
     }
 
-
+    public static String getSearchResults(Context context, String searchTerms){
+        String url = "http://izeat.r2.enst.fr/ws/Izeat/webresources/product/search/" + searchTerms;
+        return getRequestServer(context, url);
+    }
 }
