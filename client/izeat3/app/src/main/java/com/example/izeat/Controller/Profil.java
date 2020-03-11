4005864b4013763dbffc51ca70d275adb3a19437
Vincent Josse <vincent.josse@telecom-paris.fr>
2020-03-11 11:21:10 +0100

    Working get request on server
    Former-commit-id: 1480da1c36779cbe5edea5b838700bc15a9dea37

--- client/izeat3/app/src/main/java/com/example/izeat/Controller/Profil.java
+++ client/izeat3/app/src/main/java/com/example/izeat/Controller/Profil.java
@@ -9,6 +9,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.izeat.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -27,6 +33,34 @@ public class Profil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
 
+        /*
+         *  BEGINING OF NETWORK REQUEST SETUP
+         */
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://izeat.r2.enst.fr/ws/Izeat/webresources/product/3017760589895";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        System.out.println("Response is: "+ response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("######################### That didn't work! ##################");
+                System.out.println(error.getMessage());
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        /*
+         *  END OF NETWORK REQUEST SETUP
+         */
 
         this.btnRecipes= (ImageView) findViewById(R.id.btnRecipes);
         btnRecipes.setOnClickListener(new View.OnClickListener(){
@@ -74,31 +108,6 @@ public class Profil extends AppCompatActivity {
                 btnAdd.setVisibility(View.GONE);
                 btnsearch.setVisibility(View.VISIBLE);
                 btnphoto.setVisibility(View.VISIBLE);
-                final TextView textView = (TextView) findViewById(R.id.text);
-// ...
-
-// Instantiate the RequestQueue.
-                Volley.RequestQueue queue = Volley.newRequestQueue(this);
-                String url ="http://www.google.com";
-
-// Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                                textView.setText("Response is: "+ response.substring(0,500));
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        textView.setText("That didn't work!");
-                    }
-                });
-
-// Add the request to the RequestQueue.
-                queue.add(stringRequest);
-
 
             }
         });
