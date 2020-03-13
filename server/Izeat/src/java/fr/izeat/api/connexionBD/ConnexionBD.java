e5a1960cb8f84d7c1d7452d377ec0301d8f7efc4
Vincent Josse <vincent.josse@telecom-paris.fr>
2020-03-13 10:23:12 +0100

    Quick changes, need to fix bugs

--- server/Izeat/src/java/fr/izeat/api/connexionBD/ConnexionBD.java
+++ server/Izeat/src/java/fr/izeat/api/connexionBD/ConnexionBD.java
@@ -35,7 +35,7 @@ public class ConnexionBD {
         try{
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Driver ok");
-            String url="jdbc:mysql://127.0.0.1:3306/izeat?autoReconnect=true&useSSL=false";
+            String url="jdbc:mysql://127.0.0.1/izeat?autoReconnect=true&useSSL=false";
             String userName="vince";
             String password="tretre3.14";
                 System.out.println("In process");
